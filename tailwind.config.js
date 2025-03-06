f21167642731bf996e28d77694c18722424a9703
zeineb-m <zeineb.maatalli@esprit.tn>
2025-03-06 21:42:55 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4169E1',
+        'primary-dark': '#3153B6',
+        'page-bg': '#F7F3EE',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
